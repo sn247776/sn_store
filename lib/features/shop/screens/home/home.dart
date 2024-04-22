5596dd76d6a30f36888b4d154a98373c1a2c4026
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2024-04-22 21:54:48 +0530

    added curved edge

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sn_store/common/widgets/custom_shapes/circular_container.dart';
+import 'package:sn_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:sn_store/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,16 +12,18 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: SnColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(top: -150, right: -250, child: SnCircularContainer(backgroundColor: SnColors.textWhite.withOpacity(0.1),),),
-                    Positioned(top: 100, right: -300, child: SnCircularContainer(backgroundColor: SnColors.textWhite.withOpacity(0.1),),),
-                  ],
+            SnCurvedEdgeWidget(
+              child: Container(
+                color: SnColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(top: -150, right: -250, child: SnCircularContainer(backgroundColor: SnColors.textWhite.withOpacity(0.1),),),
+                      Positioned(top: 100, right: -300, child: SnCircularContainer(backgroundColor: SnColors.textWhite.withOpacity(0.1),),),
+                    ],
+                  ),
                 ),
               ),
             )
@@ -30,5 +33,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
 
 
