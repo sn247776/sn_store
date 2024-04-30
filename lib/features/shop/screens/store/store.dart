19b79a7b2116d082ccc428e187282361f54c2331
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2024-04-30 21:05:12 +0530

    Update store.dart

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,18 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:sn_store/common/products/cart/cart_menu_icon.dart';
 import 'package:sn_store/common/products/products/brand_card.dart';
 import 'package:sn_store/common/widgets/appbar/appbar.dart';
 import 'package:sn_store/common/widgets/custom_shapes/containers/search_container.dart';
-import 'package:sn_store/common/widgets/custom_shapes/rounded_container.dart';
-import 'package:sn_store/common/widgets/images/sn_circular_image.dart';
 import 'package:sn_store/common/widgets/layout/grid_layout.dart';
 import 'package:sn_store/common/widgets/texts/section_heading.dart';
-import 'package:sn_store/common/widgets/texts/sn_brand_title_text_with_verified_icon.dart';
 import 'package:sn_store/utils/constants/colors.dart';
-import 'package:sn_store/utils/constants/enums.dart';
-import 'package:sn_store/utils/constants/image_strings.dart';
 import 'package:sn_store/utils/constants/sizes.dart';
 import 'package:sn_store/utils/helpers/helper_functions.dart';
 
@@ -21,70 +14,86 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: SnAppBar(
-        title: Text(
-          'Store',
-          style: Theme.of(context).textTheme.headlineMedium,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: SnAppBar(
+          title: Text(
+            'Store',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: [
+            SnCartCounterIcon(onPressed: () {}),
+          ],
         ),
-        actions: [
-          SnCartCounterIcon(onPressed: () {}),
-        ],
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (_, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              automaticallyImplyLeading: false,
-              pinned: true,
-              floating: true,
-              backgroundColor: SnHelperFunctions.isDarkMode(context)
-                  ? SnColors.black
-                  : SnColors.white,
-              expandedHeight: 440,
-              flexibleSpace: Padding(
-                padding: const EdgeInsets.all(SnSizes.defaultSpace),
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    const SizedBox(
-                      height: SnSizes.spaceBtwItems,
-                    ),
-
-                    /// -- Search bar
-                    const SnSearchContainer(
-                      text: 'Search in Store',
-                      showBorder: true,
-                      showBackground: false,
-                      padding: EdgeInsets.zero,
-                    ),
-                    const SizedBox(
-                      height: SnSizes.spaceBtwSections,
-                    ),
-
-                    /// Featured Brands
-                    SnSectionHeading(
-                      title: 'Featured Brands',
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: SnSizes.spaceBtwItems / 1.5),
-
-                    SnGridLayout(
-                        itemCount: 4,
-                        mainAxisExtent: 80,
-                        itemBuilder: (_, index) {
-                          return const SnBrandCard();
-                        })
+        body: NestedScrollView(
+          headerSliverBuilder: (_, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                pinned: true,
+                floating: true,
+                backgroundColor: SnHelperFunctions.isDarkMode(context)
+                    ? SnColors.black
+                    : SnColors.white,
+                expandedHeight: 440,
+                flexibleSpace: Padding(
+                  padding: const EdgeInsets.all(SnSizes.defaultSpace),
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      const SizedBox(
+                        height: SnSizes.spaceBtwItems,
+                      ),
+      
+                      /// -- Search bar
+                      const SnSearchContainer(
+                        text: 'Search in Store',
+                        showBorder: true,
+                        showBackground: false,
+                        padding: EdgeInsets.zero,
+                      ),
+                      const SizedBox(
+                        height: SnSizes.spaceBtwSections,
+                      ),
+      
+                      /// Featured Brands
+                      SnSectionHeading(
+                        title: 'Featured Brands',
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: SnSizes.spaceBtwItems / 1.5),
+      
+                      SnGridLayout(
+                          itemCount: 4,
+                          mainAxisExtent: 80,
+                          itemBuilder: (_, index) {
+                            return const SnBrandCard();
+                          })
+                    ],
+                  ),
+                ),
+      
+                /// Tabs
+                bottom: TabBar(
+                  isScrollable: true,
+                  indicatorColor: SnColors.primary,
+      
+                  tabs: [
+                    Tab(child: Text('Sports')),
+                    Tab(child: Text('Funiture')),
+                    Tab(child: Text('Electronics')),
+                    Tab(child: Text('Clothes')),
+                    Tab(child: Text('Cosmetics')),
                   ],
                 ),
-              ),
-            )
-          ];
-        },
-        body: Container(),
+              )
+            ];
+          },
+          body: Container(),
+        ),
       ),
     );
   }
 }
-
